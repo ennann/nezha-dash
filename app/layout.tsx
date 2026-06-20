28a951becdeac09c6107a909c57c285b515a3088
ennann <hi@yizhe.me>
2026-06-20 10:09:05 +0800

    🎨 style: 替换状态页站点图标

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,14 @@ export const metadata: Metadata = {
   manifest: "/manifest.json",
   title: customTitle || "NezhaDash",
   description: customDescription || "A dashboard for nezha",
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
+  },
   appleWebApp: {
     capable: true,
     title: customTitle || "NezhaDash",
