18fb07b6404e7929590e3310737cc82e3d8c394d
ennann <hi@yizhe.me>
2026-06-20 09:59:21 +0800

    🎨 style: 调整状态行首列比例

--- components/ServerCardInline.tsx
+++ components/ServerCardInline.tsx
@@ -30,10 +30,10 @@ export default function ServerCardInline({
     <Link onClick={saveSession} href={`/server/${id}`} prefetch={true}>
       <Card
         className={cn(
-          "flex items-center lg:flex-row justify-start gap-3 p-3 md:px-5 cursor-pointer hover:bg-accent/50 transition-colors min-w-[940px] w-full",
+          "flex items-center lg:flex-row justify-start gap-3 p-3 md:px-5 cursor-pointer hover:bg-accent/50 transition-colors min-w-[936px] w-full",
         )}
       >
-        <section className={cn("flex w-[200px] flex-col gap-1")}>
+        <section className={cn("flex w-[176px] shrink-0 flex-col gap-1")}>
           <div
             className="grid items-center gap-2"
             style={{ gridTemplateColumns: "auto auto 1fr" }}
@@ -63,10 +63,10 @@ export default function ServerCardInline({
             tagsClassName="flex-nowrap overflow-hidden"
           />
         </section>
-        <Separator orientation="vertical" className="h-8 mx-0 ml-2" />
+        <Separator orientation="vertical" className="h-8 mx-0" />
         <div className="flex flex-col gap-1">
           <section className={cn("grid grid-cols-9 items-center gap-3 flex-1")}>
-            <div className={"flex w-14 flex-col"}>
+            <div className={"flex w-16 flex-col"}>
               <BillingSummary
                 serverInfo={serverInfo}
                 compact
@@ -135,10 +135,10 @@ export default function ServerCardInline({
   ) : (
     <Card
       className={cn(
-        "flex items-center justify-start gap-3 p-3 md:px-5 min-h-[61px] min-w-[940px] flex-row",
+        "flex items-center justify-start gap-3 p-3 md:px-5 min-h-[61px] min-w-[936px] flex-row",
       )}
     >
-      <section className={cn("flex w-[200px] flex-col gap-1")}>
+      <section className={cn("flex w-[176px] shrink-0 flex-col gap-1")}>
         <div
           className="grid items-center gap-2"
           style={{ gridTemplateColumns: "auto auto 1fr" }}
